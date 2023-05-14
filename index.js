17aa9631d07b0dc1cad61a702ec47f041201984b
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-14 19:37:32 +0600

    custom booking of each user added

--- index.js
+++ index.js
@@ -44,13 +44,26 @@ async function run() {
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const options = { projection: { title: 1, price: 1, service_id: 1 } };
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
       const result = await serviceCollection.findOne(query, options);
 
       res.send(result);
     });
 
     // Booking
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+
+      const result = await bookingCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
